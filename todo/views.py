988f57757bbf57a286da113aecf6dde4c213699d
Piotr Pepliński <p.peplaj@gmail.com>
2020-10-26 17:06:26 +0100

    created sign up function - user creation

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.db import IntegrityError
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'todo/home.html')
+
+
+def sign(request):
+    if request.method == 'GET':
+        return render(request, 'todo/sign.html', {'form': UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(
+                    request.POST['username'], "", request.POST['password1'])
+            except IntegrityError:
+                error = "This username is already taken. Try different one."
+                return render(request, 'todo/sign.html',
+                          {'error': error, 'form': UserCreationForm()})
+            else:
+                user.save()
+                login(request, user)
+                return redirect(home)
+        else:
+            error = "Passwords didn't match. Try again."
+            return render(request, 'todo/sign.html',
+                          {'error': error, 'form': UserCreationForm()})
--- todoManager/urls.py
+++ todoManager/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # todos
+    path('', views.home, name='home'),
+
+    # auth
+    path('sign', views.sign, name='sign'),
 ]
