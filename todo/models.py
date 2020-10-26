192efb67d4077261f8996d75c46404296d621043
Piotr Pepliński <p.peplaj@gmail.com>
2020-10-26 18:01:55 +0100

    created todo model. added readonly field(createDate) to admin panel

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from django.db.models import *
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Todo(Model):
+    title = CharField(max_length=100)
+    memo = TextField(blank=True)
+    importance = BooleanField(default=False)
+    createDate = DateTimeField(auto_now_add=True)
+    completeDate = DateTimeField(blank=True, null=True)
+    user = ForeignKey(User, on_delete=CASCADE)
+
+    def __str__(self):
+        return self.title
