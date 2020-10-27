257a77d121da11051a65c76cde883d3e0d946247
Piotr Pepliński <p.peplaj@gmail.com>
2020-10-27 13:06:24 +0100

    started layout proccess - detail page looks presentable

--- todo/forms.py
+++ todo/forms.py
@@ -1,8 +1,13 @@
 from django.forms import ModelForm
 from .models import Todo
+from django import forms
 
 
 class TodoForm(ModelForm):
     class Meta:
         model = Todo
         fields = ('title', 'memo', 'importance')
+        widgets = {
+            'memo': forms.Textarea(attrs={'rows': 4, 'cols': 38}),
+            'title': forms.Textarea(attrs={'rows': 1, 'cols': 38})
+        }
